dadb0fc9c23b8e9752b0b7b98a97cd731234083a
Luca <you@example.com>
2024-11-03 12:50:14 +0100

    Udate

---
+++ public/java/script.js
@@ -0,0 +1,3 @@
+document.getElementById("meinButton").addEventListener("click", function() {
+    alert("Hello World");
+});
--- public/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("meinButton").addEventListener("click", function() {
-    alert("Hello World");
-});
